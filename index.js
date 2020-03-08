2b9f39f41e9e4968d5287085c8df386a1e361fa5
Mike <gimp2312@gmail.com>
2020-03-08 20:47:28 +0100

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "semi": ["error", "never", { 
+      "beforeStatementContinuationChars": "always" 
+    }],
+    "quotes": ["error", "single", { "avoidEscape": true }],
+    "prettier/prettier": 
+      [
+        "error",
+        {
+          singleQuote: true,
+          trailingComma: "all"
+        }
+      ],
+    "import/no-unresolved": "off",
+    "import/extensions": [2, "never", { "mjs": "always"}],
+    "import/prefer-default-export": "off",
+    "func-names": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  }
+}
